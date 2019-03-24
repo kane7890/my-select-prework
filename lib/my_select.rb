38e6a3ab66ba6d433d0dd2413d0f473c53ff81e4
Kane Koo <kanekoo@gmail.com>
2019-03-24 23:00:06 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+  i=0
+  j=0
+  clength=collection.length
+  collec2=[]
+  if clength > 0 
+  while i < clength 
+     collec2[j] = yield(collection[i])
+     
+     i += 1
+     j += 1
+    end
+  end
+  collec2
+  # code here
+end
 end
